4ab800842a167ec2e69379b7dfc4a8e75df633b0
irvan ramadan <ir.irvanramadan@gmail.com>
2022-11-12 17:27:50 +0700

    spring initializr

---
+++ src/test/java/mii/security/serverside/security/login/SecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package mii.security.serverside.security.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
